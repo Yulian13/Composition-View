28b8709cf8493e1c390a676bab87c2811845aa84
shmigol2001 <shmigol2001@gmail.com>
2021-03-11 22:12:44 +0300

    +Adding

--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -20,6 +20,25 @@ namespace Composition_View.Forms
 
         private const string Connect = @"Data Source=DESKTOP-HI8BT21\SQLEXPRESS;Database=Composition;Trusted_Connection=True;AttachDbFileName=D:\Anime\DataBase\Composition.mdf";
         static PhotoContext db;
+        public MainForm()
+        {
+            InitializeComponent();
+
+            try
+            {
+                db = new PhotoContext(Connect);
+                db.Compositions.Load();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex}");
+                return;
+            }
+
+            dataGridViewComposition.DataSource = db.Compositions.Local.ToBindingList();
+
+            CallInputKey();
+        }
 
         int PastDataRowIndex = -1;
         int NowId
@@ -42,29 +61,6 @@ namespace Composition_View.Forms
 
                 return id;
             }
-        }
-
-        bool RightKey { get; set; }
-        private string Key;
-
-        public MainForm()
-        {
-            InitializeComponent();
-
-            try
-            {
-                db = new PhotoContext(Connect);
-                db.Compositions.Load();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error: {ex}");
-                return;
-            }
-
-            dataGridViewComposition.DataSource = db.Compositions.Local.ToBindingList();
-
-            CallInputKey();
         }
 
         private void CallGridView_SelectionChanged(object sender = null, EventArgs e = null) => dataGridView_SelectionChanged(sender, e);
@@ -102,6 +98,9 @@ namespace Composition_View.Forms
 
         #region Buttons of options 
 
+        bool RightKey { get; set; }
+        private string Key;
+
         private void CallInputKey(object sender = null, EventArgs e = null) => changeKeyToolStripMenuItem_Click(sender, e);
         private void changeKeyToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -111,7 +110,7 @@ namespace Composition_View.Forms
                 this.Key = new string(input.GetText.ToCharArray());
             }
 
-            buttonAdd.Enabled = !String.IsNullOrWhiteSpace(this.Key);
+            buttonOpen.Enabled = !String.IsNullOrWhiteSpace(this.Key);
 
             if (sender != null & e != null)
                 CallGridView_SelectionChanged();
@@ -164,5 +163,152 @@ namespace Composition_View.Forms
             OrigNameDataGridViewTextBoxColumn.Visible = checkBox.Checked;
             nameDataGridViewTextBoxColumn.Visible     = !checkBox.Checked;
         }
+
+        private void buttonGetRandom_Click(object sender, EventArgs e)
+        {
+            var rn = new Random();
+            int number;
+
+            do
+                number = rn.Next(dataGridViewComposition.RowCount);
+            while (dataGridViewComposition.CurrentRow.Index == number);
+
+            dataGridViewComposition.CurrentCell = dataGridViewComposition[idDataGridViewTextBoxColumn.Index, number];
+        } //Get random Row
+
+        bool usedBackgroundWorker = false;
+        public bool UsedBackgroundWorker
+        {
+            get => usedBackgroundWorker;
+            set
+            {
+                usedBackgroundWorker = value;
+
+                progressBar1.Visible = usedBackgroundWorker;
+                buttonCancel.Visible = usedBackgroundWorker;
+
+                buttonAdd.Enabled = !usedBackgroundWorker;
+                //buttonExport.Enabled = !usedBackgroundWorker;
+                //changeCompositionsKeyToolStripMenuItem.Enabled = !usedBackgroundWorker;
+
+                if (!usedBackgroundWorker) progressBar1.Value = 0;
+            }
+        }
+
+        #region Adding
+
+        static Composition composition;
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog FilePath = new FolderBrowserDialog();
+            if (FilePath.ShowDialog() != DialogResult.OK)
+                return;
+            backgroundWorkerAdding.RunWorkerAsync(FilePath.SelectedPath);
+            UsedBackgroundWorker = true;
+
+        }
+
+        private void backgroundWorkerAdding_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string Key = new string(this.Key.ToCharArray());
+            string FilePath = (string)e.Argument;
+            string[] imagesPath = Directory.GetFiles(FilePath, "*jpg", SearchOption.TopDirectoryOnly);
+            if (imagesPath.Length == 0)
+                imagesPath = Directory.GetFiles(FilePath, "*png", SearchOption.TopDirectoryOnly);
+
+            Image[] Images = null;
+
+            try
+            {
+                Images = new Image[imagesPath.Length];
+                for (int i = 0; i < imagesPath.Length; i++)
+                {
+                    Images[i] = Image.FromFile(imagesPath[i]);
+                    if (Images[i].Size.Height < 32 && Images[i].Size.Width < 32)
+                        throw new Exception($"Image №{i} is too small");
+                }
+            }
+            catch (OutOfMemoryException)
+            {
+                MessageBox.Show("Error");
+                return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            if (Images.Length == 0) throw new Exception("there isn't images");
+
+            string[] Files = FilePath.Split('\\');
+            string DirectoryName = Librari.Shifrovka(Files.Last(), Key);
+            Files = null;
+
+            Random rndm = new Random();
+            composition = new Composition() { Name = DirectoryName, NumberPhotos = Images.Length };
+
+            for (int a = 0; a < Images.Length; a++)
+            {
+                if (backgroundWorkerAdding.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+
+                backgroundWorkerAdding.ReportProgress(Images.Length);
+
+                NewImage myBitmaps = new NewImage(Images[a], rndm);
+
+                myBitmaps.ShifrovkaImage();
+
+                Photo photo = new Photo()
+                {
+                    Image = Librari.imageToByteArray(myBitmaps.image),
+                    RightKey = Librari.Shifrovka(myBitmaps.RightKey, Key),
+                    Composition = composition
+                };
+                composition.Photos.Add(photo);
+
+                backgroundWorkerAdding.ReportProgress(Images.Length);
+
+            }
+        }
+
+        private void backgroundWorkerAdding_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Maximum = e.ProgressPercentage * 2;
+            progressBar1.Value++;
+        }
+
+        private void backgroundWorkerAdding_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+            if (e.Error != null)
+            {
+                MessageBox.Show("Error!: " + e.Error.Message);
+            }
+            else if (e.Cancelled == false)
+            {
+                foreach (var photo in composition.Photos)
+                    db.Photos.Add(photo);
+                db.Compositions.Add(composition);
+
+                db.SaveChanges();
+
+                composition.IdFirstPhoto = composition.Photos.First().Id;
+
+                db.SaveChanges();
+
+                MessageBox.Show("Composition have been added");
+            }
+
+            UsedBackgroundWorker = false;
+
+            composition = null;
+        }
+
+        #endregion
+
     }
 }
